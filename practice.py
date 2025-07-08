5a62cc34254d2bf2f34007ea3e91485c5f2f61c2
Shreyanshh1400 <shreyansh1410ranjan@gmail.com>
2025-07-08 21:51:35 +0530

    Shreyansh

--- practice.py
+++ practice.py
@@ -1,4 +1,15 @@
 # user_input = input("Enter any character")
 # print(F"Character you entered= {user_input}")
 # print (F"Type of data= {type(user_input)}")
-print("Hi")
+# print("Hi")
+
+# unit= int(input("Number of units consumed="))
+# if unit<=100:
+#     bill=0
+#     print("Bill= ", bill)
+# elif unit>100 and unit<=200:
+#     bill= (unit-100)*5
+#     print("Bill=", bill)
+# else:
+#     bill=((unit-200)*10)+500
+#     print("Bill= ", bill)
