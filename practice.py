fcf96c349d55ce37f6d8fa9fd8b179e2b3d790bb
Shreyanshh1400 <shreyansh1410ranjan@gmail.com>
2025-07-08 22:13:51 +0530

    Shreyansh

--- practice.py
+++ practice.py
@@ -13,3 +13,33 @@
 # else:
 #     bill=((unit-200)*10)+500
 #     print("Bill= ", bill)
+
+# phnumber=int(input("Enter phone number="))
+# print("Last digit of the number is= ", phnumber%10)
+
+# phnumber=int(input("Enter the phone number="))
+# lstdigit=phnumber%10
+# if lstdigit%3==0:
+#     print("The last digit is divisible by 3")
+# else:
+#     print("The last digit is not divisible by 3")
+
+# percentage=float(input("Enter percentage scored by student="))
+# if percentage>90:
+#     print("Grade=A")
+# elif percentage>80 and percentage<=90:
+#     print("Grade=B")
+# elif percentage>60 and percentage<=80:
+#     print("Grade= C")
+# else:
+#     print("Grade= D")
+
+# cp= int(input("Enter the cost price of the bike="))
+# if cp>100000:
+#     tax= (15/100)*cp
+# elif cp>50000 and cp<=100000:
+#     tax=(10/100)*cp
+# else:
+#     tax=(5/100)*cp
+# print("Tax= ", tax)
+
