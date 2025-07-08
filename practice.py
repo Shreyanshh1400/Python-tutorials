2ccae078254c8f3990ec45bddc3172e07d235543
Shreyanshh1400 <shreyansh1410ranjan@gmail.com>
2025-07-09 00:21:13 +0530

    Shreyansh

--- practice.py
+++ practice.py
@@ -43,3 +43,110 @@
 #     tax=(5/100)*cp
 # print("Tax= ", tax)
 
+# num1 = int(input("Enter any number between 1 to 7="))
+# if num1==1:
+#     print("Sunday")
+# elif num1==2:
+#     print("Monday")
+# elif num1==3:
+#     print("Tuesday")
+# elif num1==4:
+#     print("Wednesday")
+# elif num1==5:
+#     print("Thursday")
+# elif num1==6:
+#     print("Friday")
+# elif num1==7:
+#     print("Saturday")
+
+# num1 = int(input("Enter any number between 1 to 12"))
+# if num1==1:
+#     print("January and it has 31 days")
+# elif num1==2:
+#     print("February and it has 28 days")
+# elif num1==3:
+#     print("March and it has 31 days")
+# elif num1==4:
+#     print("April and it has 30 days")
+# elif num1==5:
+#     print("May and it has 31 says")
+# elif num1==6:
+#     print("June and it has 30 days")
+# elif num1==7:
+#     print("July and 31 days")
+# elif num1==8:
+#     print("August and it has 31 days")
+# elif num1==9:
+#     print("September and it has 30 days")
+# elif num1==10:
+#     print("October and it has 31 days")
+# elif num1==11:
+#     print("November and it has 30 days")
+# elif num1==12:
+#     print("December and it has 31 days")
+# else:
+#     print("Please enter number betweeen 1 and 12")
+
+# city_name=input("Enter the city name")
+# if city_name.lower()=="delhi":
+#     print("Monument name is Red fort")
+# elif city_name.lower()=="agra":
+#     print("Monument name is Taj mahal")
+# elif city_name.lower()=="jaipur":
+#     print("Monument name is Jal mahal")
+# else:
+#     print("No monument in the city")
+
+# age= int(input("Enter age"))
+# if age>=18:
+#     print("The person is eligible to vote")
+# else:
+#     print("The person is not eligible to vote")
+
+# age=int(input("Enter age of the person"))
+# if age>=60:
+#     print("The person is senior citizen")
+# else:
+#     print("The person is not senior citizen")
+
+# num1=int(input("Enter the first number"))
+# num2=int(input("Enter the second number"))
+# if num1>num2:
+#     print(num2," is the lowest value")
+# else:
+#     print(num1," is the lowest value")
+
+# num1=int(input("Enter the first number"))
+# num2=int(input("Enter the second number"))
+# if num1>num2:
+#     print(num1," is the higher value")
+# else:
+#     print(num2," is the higher value")
+
+# num=int(input("Enter any number"))
+# if num>0:
+#     print("It is +ve value")
+# else:
+#     print("It is -ve value")
+
+# num=int(input("Enter any number"))
+# if num%2==0:
+#     print("It is even number")
+# else:
+#     print("It is odd number")
+
+# num=int(input("Enter any number"))
+# if num%2==0 and num%3==0:
+#     print(num," is divisible by 2 and 3")
+# else:
+#     print(num," is not divisivle by 2 and 3")
+
+# num1=int(input("Enter 1st number"))
+# num2=int(input("Enter 2nd number"))
+# num3=int(input("Enter 3rd value"))
+# if num1>num2 and num1>num3:
+#     print(num1," is the greatest number")
+# elif num2>num1 and num2>num3:
+#     print(num2," is the greatest number")
+# else:
+#     print(num3," is the greatest number")
