6c2f00ea18a3943092d896445224f33613948f1a
Shreyanshh1400 <shreyansh1410ranjan@gmail.com>
2025-07-15 00:38:27 +0530

    first

--- practice.py
+++ practice.py
@@ -150,3 +150,379 @@
 #     print(num2," is the greatest number")
 # else:
 #     print(num3," is the greatest number")
+
+# # print  ("  ///// ")
+# # print  (' +"""""+ ')
+# # print('(| o o |)')
+# # print (" |  ^  |")
+# # print  (" +-----+") 
+
+
+
+# # print('     +    ')
+# # print('   +   +  ') 
+# # print('  +     +  ')
+# # print('+---------+')
+# # print('|   .-.    |')
+# # print('|   | |    |')
+# # print('+-+-+-+-+-+')
+
+
+
+# # print('   /\_/\\       -------')
+# # print ('  ( .  .)     /Hello \\')
+# # print ('  (  -  )     <Senior |')
+# # print('  |  |  |     \\Coder!/')
+# # print('  (__|__)        -----')
+
+
+# # #Multiplication
+
+# # num1= int(input("Enter the first number."))
+# # num2=int(input("Enter the second number."))
+# # product= num1 * num2
+# # print("Produt of the given 2 numbers=",product)
+
+
+
+# #Q1
+
+# # num1 = int(input("Enter the first number."))
+# # num2 = int(input("Enter the second number."))
+# # product = num1*num2
+# # if product<=1000:
+# #     print("Product of given 2 numbers=", product)
+# # else:
+# #     sum= num1+num2
+# #     print("Sum of 2 numbers=",sum) 
+
+# import math
+# # 1 #Number theoritic funct8ions
+# # print(math.comb(10,5))
+# # print(math.factorial(5))
+# # print(math.gcd(5,9))
+# # print(math.sqrt())
+# # print(math.perm(10,2))
+# #2 Floating point airthmetric
+# # print(math.ceil(53.3))
+# # print(math.fabs(-53.3))
+# # print(math.floor(53.4))
+# # print(math.fma(5,2,5)) # (x*y)+a
+# # print(math.fmod(10,3)) #remainder
+# # print (math.modf(5.6)) #fractional and integral part of x
+# # print (math.remainder(10,4))
+# # print(math.trunc(5.96))
+# #3 Floating point manipulation functions
+# # print(math.copysign(5,-8))
+# # print(math.frexp(5.6))
+# # print(math.isclose(2.3,5.2))
+# # print(math.isfinite(5.6954355432366554423))
+# # print(math.isinf(56))
+# # print(math.nextafter(1.1,1.3))
+# # print(math.exp2(0))
+# # print(math.expm1(0))
+# # print(math.log(5,2))
+# # print(math.log10(5))
+# # print(math.pow(2,0))
+# # for char in "Python":
+#     # print(char)
+# # count = 0
+# # while count < 5:
+# #     print(count)
+# #     count += 1
+# # for i in range(3):
+# #     for j in range(2):
+# #         print(f"i={i}, j={j}")
+# # for with range
+# # for i in range(1, 6, 2):  # Start=1, stop=6, step=2 -> 1, 3, 5
+# #     print(i)
+
+# # while with break
+# # count = 0
+# # while True:
+# #     if count == 3:
+# #         break
+# #     print(count)
+# #     count += 1
+
+# # # for with else
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Loop completed")  # Runs after loop
+
+# age =int(input("Please enter the age of person"))
+# if(age>=18):
+#     print("Person is eligile to vote")
+# else:
+#     print("Person is not eligible to vote")
+
+
+# num1=int(input("Enter any number"))
+# if(num1%2==0):
+#     print("This is an even number")
+# else:
+#     print("It is an odd number")
+
+# num1=int(input("Enter any number"))
+# if num1%7==0:
+#     print("The number is divisible by 7")
+# else:
+#     print("This number is not divisible by 7")
+
+# num1=int(input("Enter any number"))
+# if num1%5==0:
+#     print("Hello")
+# else:
+#     print("Bye")
+
+# unit_consumed= int(input("Enter the number of units consumed"))
+# if unit_consumed<=100:
+#     print("Bill")
+
+# temp=float(input("Enter the temprature of water"))
+# if temp==100.0:
+#     print("It is the boiloing point of water")
+# else:
+#     print("It is not the boiling point of water")
+
+# age1=int(input("Enter the age of first person"))
+# age2=int(input("Enter the age of second person"))
+# age3=int(input("Enter the age of third person"))
+# age4=int(input("Enter the age of fourth person"))
+# if age1<age2 and age1<age3 and age1<age4:
+#     print("Person one is the yougest whose age is ",age1)
+# elif age2<age1 and age2<age3 and age2<age4:
+#     print("Prson two is the youngest whose age is ",age2)
+# elif age3<age1 and age3<age2 and age3<age4:
+#     print("Prson third is the youngest whose age is ",age3)
+# else:
+#     print("Prson fourth is the youngest whose age is ",age4)
+
+# age1=int(input("Enter the age of first person"))
+# age2=int(input("Enter the age of second person"))
+# age3=int(input("Enter the age of third person"))
+# age4=int(input("Enter the age of fourth person"))
+# if age1>age2 and age1>age3 and age1>age4:
+#     print("Person one is the oldest whose age is ",age1)
+# elif age2>age1 and age2>age3 and age2>age4:
+#     print("Prson two is the oldest whose age is ",age2)
+# elif age3>age1 and age3>age2 and age3>age4:
+#     print("Prson third is the oldest whose age is ",age3)
+# else:
+#     print("Prson fourth is the oldest whose age is ",age4)
+
+# num=int(input("Enter any number"))
+# count=1
+# for i in range(2,num,1):
+#     if num%i==0:
+#         count+=1
+#     else:
+#         count+=0
+
+# if count==2:
+#     print("It is a prime number")
+# else:
+#     print("It is not a prime number")
+
+# entvow=input("Enter any vowel")
+# vowels="aeiouAEIOU"
+# if entvow in vowels:
+#     print("It is a vowel")
+# else:
+#     print("It is not a vowel")
+
+# twd=int(input("Enter total number of working days"))
+# ta=int(input("Enter number of days absent"))
+# percentage=((twd-ta)/twd)*100
+# print("Percent of attandence =", percentage)
+# if percentage>=75:
+#     print("Student is eligible to give exam")
+# else:
+#     print("Student is not eligible to give exam")
+
+# percent=float(input("Accept the percentage secured by student"))
+# if percent<25:
+#     print("Grade=D")
+# elif percent>=25 and percent<45:
+#     print("Grade=C")
+# elif percent>=45 and percent<50:
+#     print("Grade=B")
+# elif percent>=50 and percent<60:
+#     print("Grade=B+")
+# elif percent>=60 and percent<80:
+#     print("Grade=A")
+# else:
+#     print("Grade=A+")
+
+# service_time=int(input("Enter the number of years since you were working here"))
+# salary=float(input("Enter your salary"))
+# if service_time>10:
+#     bonus= (10/100)*salary
+# elif service_time>=6 and service_time<=10:
+#     bonus=(8/100)*salary
+# elif service_time<6:
+#     bonus=(5/100)*salary
+# print("Bonus= ", bonus)
+
+# Mp= float(input("Enter the marked price"))
+# if Mp>10000:
+#     discount=(20/100)*Mp
+# elif Mp>7000 and Mp<=10000:
+#     discount=(15/100)*Mp
+# else:
+#     discount=(10/100)*Mp
+# Netamt=Mp-discount
+# print("Marked price= ", Mp)
+# print("Discount= ", discount)
+# print("Net amount= ", Netamt)
+
+# percent= float(input("Enter percentage of student"))
+# if percent<40:
+#     print("Failed")
+# elif percent>=40 and percent<55:
+#     print("Fair")
+# elif percent>=55 and percent<65:
+#     print("Good")
+# else:
+#     print("Excellent")
+
+# side1 = int(input("Length of side 1"))
+# side2 = int(input("Length of side 2"))
+# side3 = int(input("Length of side 3"))
+# if side1==side2 and side1==side3 and side2==side3:
+#     print("It is an equilateral triangle")
+# elif side1!=side2 and side1!=side3 and side2!=side3:
+#     print("It is an scalene triangle")
+# else:
+#     print("It is an isoceles triangle")
+
+# num1=int(input("Enter the first number"))
+# num2=int(input("Enter the second number"))
+# mathope=input("Enter the  sign of maths operation to be performed")
+# if mathope=='+':
+#     print(num1+num2)
+# elif mathope=='-':
+#     print(num1-num2)
+# elif mathope=='*':
+#     print(num1*num2)
+# elif mathope=='/':
+#     print(num1/num2)
+# elif mathope=='%':
+#     print(num1%num2)
+
+# age=int(input("Enter the age of person"))
+# gender=input("Enter gender of person")
+# if age>=18 and age<30:
+#     if gender.lower()=='male':
+#         print("Wage/day= 700")
+#     elif gender.lower()=='female':
+#         print("Wage/day= 750")
+# elif age>=30 and age<=40:
+#     if gender.lower()=='male':
+#         print("Wage/day= 800")
+#     elif gender.lower()=='female':
+#         print("Wage/day= 850")
+# else:
+#     print("Enter appropriate age")
+
+# num1=int(input("Enter the first number"))
+# num2=int(input("Enter the second number"))
+# num3=int(input("Enter the third number"))
+# if num1>num2 and num1>num3:
+#     if num2>num3:
+#         print("Second greatest number is ", num2)
+#     elif num3>num2:
+#         print("Second greatest number is ", num3)
+# elif num2>num1 and num2>num3:
+#     if num1>num3:
+#         print("Second greatest number is ", num1)
+#     elif num3>num1:
+#         print("Second greatest number is ", num3)
+# else:
+#     if num1>num2:
+#         print("Second greatest number is ", num1)
+#     elif num2>num1:
+#         print("Second greatest number is ", num2)
+
+# side1= int(input("Enter length of side 1"))
+# side2= int(input("Enter length of side 2"))
+# side3= int(input("Enter length of side 3"))
+# if side1+side2>side3 or side1+side3>side2 or side2+side3>side1:
+#     print("Triangle is possible")
+# else:
+#     print("Triangle is not possible")
+
+# unit=int(input("Enter number of units consumed"))
+# if unit<=100:
+#     bill=0
+# elif unit>100 and unit<=300:
+#     bill=(unit-100)*2
+# elif unit>300:
+#     bill=0+400+((unit-300)*5)
+# print("Unit consumed= ", unit)
+# print("Bill= ", bill)
+
+# nod=int(input("Enter number of days lib used "))
+# if nod<=5:
+#     bill= nod*2
+# elif nod>6 and nod<=10:
+#     bill= 10+((nod-5)*3)
+# elif nod>10 and nod<=15:
+#     bill=10+15+((nod-10)*4)
+# else:
+#     bill=10+15+20+((nod-15)*5)
+# print("Number of days lib used= ", nod)
+# print("Bill= ", bill)
+
+# kmd=int(input("Enter kms covered= "))
+# if kmd<=10:
+#     bill=11*kmd
+# elif kmd>10 and kmd <=100:
+#     bill=110+((kmd-10)*10)
+# elif kmd>100:
+#     bill=110+900+((kmd-100)*9)
+
+# eng=int(input("Enter marks in english= "))
+# maths=int(input("Enter marks in maths= "))
+# sci=int(input("Enter marks in science= "))
+# ssci=int(input("Enter marks in social science= "))
+# if eng>80 and maths>80 and sci>80 and ssci>80:
+#     print("Stream alloted --> Science")
+# elif eng>80:
+#     if maths>50 and sci>50:
+#         print ("Stream alloted --> Commerce")
+#     elif ssci>80:
+#         print("Stream alloted --> Humanities")
+
+# day= input("Which day is today")
+# age=int(input("Enter the age of person"))
+# if day.lower()=="tuesday":
+#     if age<=12:
+#         tktprice=3
+#     elif age>12 and age <=64:
+#         tktprice=8
+#     elif age>=65:
+#         tktprice=5
+# else:
+#     if age<=12:
+#         tktprice=5
+#     elif age>12 and age <=64:
+#         tktprice=10
+#     elif age>=65:
+#         tktprice=7
+# print("Price= ", tktprice)
+
+# username="Shreyanshh1400"
+# password="Shreyansh@14"
+# Entusrname=input("Enter username")
+# Entpass=input("Enter your password")
+# if Entusrname==username:
+#     if Entpass==password:
+#         print("Login successfull")
+#     else:
+#         print("Incorrect password")
+# elif Entusrname!=username:
+#     print("Incorrect username")
+# elif Entusrname!=username and Entpass!=password:
+#     print("Invalid credentials")
